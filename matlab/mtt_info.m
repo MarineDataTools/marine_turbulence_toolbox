fff5606bb588ca5377fcbed165ce4c4ccd090161
Peter Holtermann @ Lappalie <peter.holtermann@systemausfall.org>
2016-02-26 09:07:37 +0100

    mtt_info

--- matlab/mtt_info.m
+++ matlab/mtt_info.m
@@ -5,6 +5,11 @@ function P = mtt_info()
 % This is part of the ocean turbulence toolbox
 
   mtt_version = '0.01';
+  mtt_url = 'https://github.com/MarineDataTools/marine_turbulence_toolbox';
+  disp(' ')  
   disp(['This is the turbulence toolbox (mtt) version:' mtt_version]);
+  disp(' ')
+  disp(['url:' mtt_url]);
+  disp(' ')  
 
 end
